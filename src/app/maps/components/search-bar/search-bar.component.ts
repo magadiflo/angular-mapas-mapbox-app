3c1f38aaf16281c129cc96f4e96ebb50db954231
Martín <magadiflo@gmail.com>
2022-10-14 22:55:42 -0500

    Debounce Manual

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
+
+  /**
+   * * NodeJS.Timeout, viene en javascript, pero para usarlo en este proyecto hubo que 
+   * * modificar dos archivos: tsconfig.app.json y el tsconfig.json agregando
+   * * "types": [
+   * *    "node"
+   * *  ]
+   * * Luego para que establezca los cambios bajar y subir la aplicación
+   */
+  private debounceTime?: NodeJS.Timeout;
 
   constructor() { }
 
-  ngOnInit(): void {
+  onQueryChanged(query: string = '') {
+    /**
+     * * Cada vez que escribamos algo, se ejecutará este método onQueryChanged(...),
+     * * y como al this.debounceTime se le establece el setTimeout de 1 segundo, el 
+     * * if valida que ya tiene un debounceTime y lo limpia, eso ocurrirá siempre
+     * * que estemos tecleando continuamente. Ahora, cuando dejemos de teclear después
+     * * de 1 segundo es que recién se ejecutará el código dentro del setTimeout
+     * * 
+     */
+    if (this.debounceTime) clearTimeout(this.debounceTime);
+
+    this.debounceTime = setTimeout(() => {
+      console.log('Mandar este query: ', query);
+    }, 1000);
   }
 
 }
